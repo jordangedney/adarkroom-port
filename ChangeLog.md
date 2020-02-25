36d9ee21df7771d5aa3791b1e321ca82047b8b0a
Jordan Gedney <jordangedney@gmail.com>
2020-02-24 23:29:23 -0500

    ohboy

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for a-dark-room
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import Lib
+import Brick
+import Brick.Widgets.Center
+import Brick.Widgets.Border
+import Brick.Widgets.Border.Style
+
+ui :: Widget ()
+ui =
+    withBorderStyle unicode $
+    borderWithLabel (str "Hello!") $
+    (center (str "Left") <+> vBorder <+> center (str "Right"))
+
+main :: IO ()
+main = simpleMain ui
