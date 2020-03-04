d05046683170371d817f368f7f52a2f781697538
Jordan Gedney <jordangedney@gmail.com>
2020-03-04 17:45:38 -0500

    Add some builder states

--- src/Builder.hs
+++ src/Builder.hs
@@ -11,6 +11,27 @@ import GameEvent (GameEvent(BuilderUpdate, UnlockForest), updateEvents)
 import Constants (builderStateDelay, needWoodDelay)
 import Util (addEvent)
 
+-- Defined in GameTypes to avoid an import cycle:
+data BuilderState
+  = Approaching
+  | Collapsed
+  | Shivering
+  | Sleeping
+  | Helping
+  deriving (Eq, Show, Enum, Ord)
+
+builderState :: BuilderState -> String
+builderState Approaching = "the light from the fire spills from the windows, out into the dark."
+builderState Collapsed   = "a ragged stranger stumbles through the door and collapses in the corner."
+builderState Shivering   = "the stranger shivers, and mumbles quietly. her words are unintelligible."
+builderState Sleeping    = "the stranger in the corner stops shivering. her breathing calms."
+builderState Helping     = "the stranger is standing by the fire. she says she can help. says she builds things."
+
+builderSucc :: BuilderState -> BuilderState
+builderSucc Helping =  Helping
+builderSucc Sleeping = Sleeping
+builderSucc x = succ x
+
 builderAppears :: Game -> Game
 builderAppears game =
   let doNothing = game
