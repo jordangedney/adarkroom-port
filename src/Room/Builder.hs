f161baeec08d50e0246f2c5df687b94d35d3a263
Jordan Gedney <jordangedney@gmail.com>
2021-10-21 17:19:13 -0400

    stopgap

--- src/Room/Builder.hs
+++ src/Room/Builder.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
 module Room.Builder
   ( update
   , canHelp
@@ -16,6 +18,7 @@ import Shared.UI (showForestBuildings)
 import Shared.Game
 import Shared.GameEvent (GameEvent(..))
 import Shared.Constants
+import Shared.Item
 
 import Util (notifyRoom, updateEvents)
 
@@ -64,6 +67,7 @@ update game =
       getBetter = doBetter & notifyRoom (showState (view builderState doBetter))
   in if builderIsFine then doNothing else getBetter
 
+
 gatherWood :: Game -> Game
 gatherWood game =
   let gatherMoreInAwhile = game & updateEvents BuilderGathersWood builderGatherDelay
@@ -71,6 +75,221 @@ gatherWood game =
                     || view roomTemperature game == Cold
   in if tooColdToWork then gatherMoreInAwhile
      else gatherMoreInAwhile & over (stored . wood) (+ 2)
+
+-- gatherWood :: Game -> Game
+-- gatherWood = do
+--   tooColdToWork <- (\t -> (t == Freezing) || (t == Cold)) . view roomTemperature
+--   let gather = if tooColdToWork then id else over (stored . wood) (+ 2)
+--   gather . updateEvents BuilderGathersWood builderGatherDelay
+
+data CraftableCost = Static [(Item, Int)] | Dynamic (Game -> [(Item, Int)])
+
+data Craftable
+  = Building
+  { name :: String
+  , availableMsg :: String
+  , buildMsg :: String
+  , maxNum  :: Maybe Int
+  , maxMsg  :: Maybe String
+  , cost :: CraftableCost
+  }
+  | Tool
+  { name :: String
+  , buildMsg :: String
+  -- , cost :: [(Item, Int)]
+  , cost :: CraftableCost
+  }
+  | Upgrade
+  { name :: String
+  , buildMsg :: String
+  -- , cost :: [(Item, Int)]
+  , cost :: CraftableCost
+  }
+  | Weapon
+  { name :: String
+  , buildMsg :: String
+  -- , cost :: [(Item, Int)]
+  , cost :: CraftableCost
+  }
+
+-- data Craftable'
+--   -- Traps and Carts are handled elsewhere
+--   = Hut
+--   | Lodge
+--   | TradingPost
+--   | Tannery
+--   | Smokehouse
+--   | Workshop
+--   | Steelworks
+--   | Armoury
+--   | Torch
+--   | Waterskin
+--   | Cask
+--   | WaterTank
+--   | BoneSpear
+--   | Rucksack
+--   | Wagon
+--   | Convoy
+--   | LeatherArmour
+--   | IronArmour
+--   | SteelArmour
+--   | IronSword
+--   | SteelSword
+--   | Rifle
+
+getCraftable Hut = Building
+  { name         = "hut"
+  , availableMsg = "builder says there are more wanderers. says they'll work, too."
+  , buildMsg     =
+      "builder puts up a hut, out in the forest. says word will get around."
+  , maxNum       = Just 20
+  , maxMsg       = Just "no more room for huts."
+  , cost         = Dynamic (\g -> [(Wood, (view (stored . huts) g * 50) + 100)])
+  }
+getCraftable Lodge = Building
+  { name         = "lodge"
+  , availableMsg = "villagers could help hunt, given the means."
+  , buildMsg     = "the hunting lodge stands in the forest, a ways out of town."
+  , maxNum       = Just 1
+  , maxMsg       = Nothing
+  , cost         = Static [(Wood, 200), (Fur, 10), (Meat, 5)]
+  }
+getCraftable TradingPost = Building
+  { name         = "trading post"
+  , availableMsg = "a trading post would make commerce easier."
+  , buildMsg     =
+      "now the nomads have a place to set up shop, they might stick around a while."
+  , maxNum       = Just 1
+  , maxMsg       = Nothing
+  , cost         = Static [(Wood, 400), (Fur, 100)]
+  }
+getCraftable Tannery = Building
+  { name         = "tannery"
+  , availableMsg =
+      "builder says leather could be useful. says the villagers could make it."
+  , buildMsg     = "tannery goes up quick, on the edge of the village. "
+  , maxNum       = Just 1
+  , maxMsg       = Nothing
+  , cost         = Static [(Wood, 500), (Fur, 50)]
+  }
+getCraftable Smokehouse = Building
+  { name         = "smokehouse"
+  , availableMsg =
+      "should cure the meat, or it'll spoil. builder says she can fix something up."
+  , buildMsg     = "builder finishes the smokehouse. she looks hungry."
+  , maxNum       = Just 1
+  , maxMsg       = Nothing
+  , cost         = Static [(Wood, 600), (Meat, 50)]
+  }
+getCraftable Workshop = Building
+  { name         = "workshop"
+  , availableMsg = "builder says she could make finer things, if she had the tools."
+  , buildMsg     = "workshop's finally ready. builder's excited to get to it."
+  , maxNum       = Just 1
+  , maxMsg       = Nothing
+  , cost         = Static [(Wood, 800), (Leather, 100), (Scale, 10)]
+  }
+getCraftable Steelworks = Building
+  { name         = "steelworks"
+  , availableMsg = "builder says the villagers could make steel, given the tools."
+  , buildMsg     = "a haze falls over the village as the steelworks fires up."
+  , maxNum       = Just 1
+  , maxMsg       = Nothing
+  , cost         = Static [(Wood, 1500), (Iron, 100), (Coal, 100)]
+  }
+getCraftable Armoury = Building
+  { name         = "armoury"
+  , availableMsg = "builder says it'd be useful to have a steady source of bullets."
+  , buildMsg     = "armoury's done, welcoming back the weapons of the past."
+  , maxNum       = Just 1
+  , maxMsg       = Nothing
+  , cost         = Static [(Wood, 3000), (Steel, 100), (Sulphur, 50)]
+  }
+getCraftable Torch = Tool
+  { name         = "torch"
+  , buildMsg     = "a torch to keep the dark away."
+  , cost         = Static [(Wood, 1), (Cloth, 1)]
+  }
+getCraftable Waterskin = Upgrade
+  { name         = "waterskin"
+  , buildMsg     = "this waterskin'll hold a bit of water, at least."
+  , cost         = Static [(Leather, 50)]
+  }
+getCraftable Cask = Upgrade
+  { name         = "cask"
+  , buildMsg     = "the cask holds enough water for longer expeditions."
+  , cost         = Static [(Leather, 100), (Iron, 20)]
+  }
+getCraftable WaterTank = Upgrade
+  { name         = "water tank"
+  , buildMsg     = "never go thirsty again."
+  , cost         = Static [(Iron, 100), (Steel, 50)]
+  }
+getCraftable BoneSpear = Weapon
+  { name         = "bone spear"
+  , buildMsg     = "this spear's not elegant, but it's pretty good at stabbing."
+  , cost         = Static [(Wood, 100), (Teeth, 5)]
+  }
+getCraftable Rucksack = Upgrade
+  { name         = "rucksack"
+  , buildMsg     = "carrying more means longer expeditions to the wilds."
+  , cost         = Static [(Leather, 200)]
+  }
+getCraftable Wagon = Upgrade
+  { name         = "wagon"
+  , buildMsg     = "the wagon can carry a lot of supplies."
+  , cost         = Static [(Wood, 500), (Iron, 100)]
+  }
+getCraftable Convoy = Upgrade
+  { name         = "convoy"
+  , buildMsg     = "the convoy can haul mostly everything."
+  , cost         = Static [(Wood, 1000), (Iron, 200), (Steel, 100)]
+  }
+getCraftable LeatherArmour = Upgrade
+  { name         = "l armour"
+  , buildMsg     = "leather's not strong. better than rags, though."
+  , cost         = Static [(Leather, 200), (Scale, 20)]
+  }
+getCraftable IronArmour = Upgrade
+  { name         = "i armour"
+  , buildMsg     = "iron's stronger than leather."
+  , cost         = Static [(Leather, 200), (Iron, 100)]
+  }
+getCraftable SteelArmour = Upgrade
+  { name         = "s armour"
+  , buildMsg     = "steel's stronger than iron"
+  , cost         = Static [(Leather, 200), (Steel, 100)]
+  }
+getCraftable IronSword = Weapon
+  { name         = "iron sword"
+  , buildMsg     = "sword is sharp. good protection out in the wilds."
+  , cost         = Static [(Wood, 200), (Leather, 50), (Iron, 20)]
+  }
+getCraftable SteelSword = Weapon
+  { name         = "steel sword"
+  , buildMsg     = "the steel is strong, and the blade true."
+  , cost         = Static [(Wood, 500), (Leather, 100), (Steel, 20)]
+  }
+getCraftable Rifle = Weapon
+  { name         = "rifle"
+  , buildMsg     = "black powder and bullets, like the old days."
+  , cost         = Static [(Wood, 200), (Steel, 50), (Sulphur, 50)]
+  }
+
+--unlockCraftables :: Game -> Game
+--unlockCraftables = do
+--  where
+--        needsWorkshop Hut = False
+--        needsWorkshop Lodge = False
+--        needsWorkshop TradingPost = False
+--        needsWorkshop Tannery = False
+--        needsWorkshop Smokehouse = False
+--        needsWorkshop Workshop = False
+--        needsWorkshop Steelworks = False
+--        needsWorkshop Armoury = False
+--        needsWorkshop _ = True
+
+
 
 canBuildTraps :: Game -> Game
 canBuildTraps game =
--- src/Shared/Item.hs
+++ src/Shared/Item.hs
@@ -6,5 +6,41 @@ module Shared.Item where
 import GHC.Generics
 import Data.Yaml
 
-data Item = Fur | Cloth | Scale | Teeth | Bait | Compass | Wood | Hut
+data Item
+  = Fur
+  | Cloth
+  | Scale
+  | Teeth
+  | Bait
+  | Compass
+  | Wood
+  | Meat
+  | Coal
+  | Leather
+  | Iron
+  | Steel
+  | Sulphur
+  -- Craftable:
+  | Hut
+  | Lodge
+  | TradingPost
+  | Tannery
+  | Smokehouse
+  | Workshop
+  | Steelworks
+  | Armoury
+  | Torch
+  | Waterskin
+  | Cask
+  | WaterTank
+  | BoneSpear
+  | Rucksack
+  | Wagon
+  | Convoy
+  | LeatherArmour
+  | IronArmour
+  | SteelArmour
+  | IronSword
+  | SteelSword
+  | Rifle
   deriving (Eq, Show, Ord, Generic, ToJSON, FromJSON)
