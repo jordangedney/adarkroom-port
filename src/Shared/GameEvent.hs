5085a294552f56de7c4a6f19f9b7ab413926d54c
Jordan Gedney <jordangedney@gmail.com>
2022-09-01 09:20:12 -0400

    ⛳

--- src/Shared/GameEvent.hs
+++ src/Shared/GameEvent.hs
@@ -8,6 +8,7 @@ import GHC.Generics
 import Data.Yaml
 import Data.Aeson.Types (ToJSONKey, FromJSONKey)
 import qualified Data.Map as Map
+import Control.Lens ((&))
 
 data GameEvent
   = UnlockForest
@@ -22,16 +23,9 @@ data GameEvent
 
 -- Hacky, but >0 means active, 0 triggers, and <0 means inactive
 gameEventsInit :: Map.Map GameEvent Int
-gameEventsInit = Map.fromList
-  [ (UnlockForest,       -1)
-  , (GatherWood,         -1)
-  , (CheckTraps,         -1)
-  , (FireStoked,         -1)
-  , (FireShrinking,       1)
-  , (BuilderUpdate,      -1)
-  , (RoomChanged,         1)
-  , (BuilderGathersWood, -1)
-  ]
+gameEventsInit = Map.fromList [(e, -1) | e <- enumFrom (toEnum 0)]
+  & Map.insert FireShrinking 1
+  & Map.insert RoomChanged 1
 
 -- Helper Functions ------------------------------------------------------------
 tickEvents :: Map.Map k Int -> Map.Map k Int
